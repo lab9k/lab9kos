2feceecaab1fac2bbb0e7535a537b65ed721603f
wim de troyer <wim de troyer>
2017-08-07 10:16:03 +0200

    Add project files.

---
+++ lab9kos/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab9kos.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
