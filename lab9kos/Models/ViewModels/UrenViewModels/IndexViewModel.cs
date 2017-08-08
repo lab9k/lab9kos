1e46ef8fdf518c180bbfb8b6d37b67830619c2db
Jefwillems <jefwillems100@gmail.com>
2017-08-08 16:55:17 +0200

    IndexViewModel added, not completed

--- lab9kos/Models/ViewModels/UrenViewModels/IndexViewModel.cs
+++ lab9kos/Models/ViewModels/UrenViewModels/IndexViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace lab9kos.Models.ViewModels.UrenViewModels
+﻿using System.Collections.Generic;
+using lab9kos.Models.Domain;
+
+namespace lab9kos.Models.ViewModels.UrenViewModels
 {
     public class IndexViewModel
     {
-        
+        public ICollection<Gebruiker> Gebruikers { get; set; }
     }
 }
